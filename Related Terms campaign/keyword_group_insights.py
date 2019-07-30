2ba1db489777b1e922ada2ad1edbc7f5846b449e
amach99 <17macharla99@gmail.com>
2019-07-30 13:39:53 -0400

    STEP 1 of Related insights project: group keywords into clusters of related terms

---
+++ Related Terms campaign/keyword_group_insights.py
@@ -0,0 +1,144 @@
+food = ['Whey',
+        'Cookie',
+        'Frito-Lay',
+        'Potato chip',
+        'Barley',
+        'Low-carbohydrate diet',
+        "Lay's",
+        'Fried chicken',
+        'aquafina',
+        'pepsi',
+        'kurkure',
+        'mountain dew',
+        'pepsico',
+        'fanta',
+        'soft drinks',
+        'biscuits',
+        "Mcdonald's",
+        'pineapple',
+        'sandwich',
+        'bottled water',
+        'kale',
+        'coffeemaker',
+        'tap water',
+        'cortado',
+        'ristretto',
+        'watermelon',
+        'ketchup',
+        'cream soda',
+        'pomegranate',
+        'kiwi fruit',
+        'nestle',
+        'chocolate chip',
+        'lemon',
+        'coca cola zero',
+        'minute maid',
+        'Nestle',
+        'Sprite',
+        'soft drinks',
+        'doritos',
+        'gatorade',
+        'tropicana',
+        'coca-cola',
+        'schweppes',
+        'diet coke',
+        'powerade',
+        'thumbs up'
+        ]
+
+sports = ['Sachin Tendulkar',
+          'Virat Kohli',
+          'Cristiano Ronaldo',
+          'Ronaldinho',
+          'FIFA World Cup',
+          'Brazil national football team',
+          'Indian national cricket team',
+          'Argentina national football team',
+          'Mahendra Singh Dhoni',
+          'Juventus F.C.',
+          'Cricket World Cup',
+          'China national football team',
+          'football'
+          'copa del rey'
+          ]
+
+entertainers = ['Katie Holmes',
+                'Leonardo DiCaprio',
+                'Nelly',
+                'Lana Del Rey',
+                'Logan Paul',
+                'AC/DC',
+                'Jennifer Lawrence',
+                'KSIOlajidebt',
+                'Jason Momoa',
+                'Morena Baccarin'
+                ]
+
+movies = ['Inception'
+          'Doctor Strange',
+          'The Matrix',
+          'Hulk (comics)',
+          'Avengers (comics)',
+          'DEADPOOL: The Movie',
+          'Marvel Studios',
+          'Star Trek',
+          'The Hunger Games',
+          'Spider-Man 3',
+          'Spider-Man',
+          'WALL-E',
+          'Spider-Man 2',
+          'Spider-Man (2002 film)',
+          'Star Trek: The Original Series',
+          'The Hunger Games (film)',
+          'Iron Man 2',
+          'Logan',
+          'Iron Man',
+          'The Amazing Spider-Man (2012 film)',
+          'Deadpool',
+          'The Avengers (2012 film)',
+          'Marvel Entertainment',
+          'Ghost Rider',
+          'Fantastic Four',
+          'Black Panther (comics)',
+          'Thor (film)',
+          'The Wolverine (film)',
+          'Iron Man 3',
+          'Captain America: The Winter Soldier',
+          'Guardians of the Galaxy (film)',
+          'Avengers: Age of Ultron',
+          'Ant-Man (film)',
+          'Fantastic Beasts and Where to Find Them (film)',
+          'The Amazing Spider-Man 2',
+          'The karate kid'
+          'Ghostbusters'
+          ]
+
+fictional_char = ['Wolverine (character)',
+                  'Luke Skywalker',
+                  'Doctor Strange',
+                  'Iron Man',
+                  'Hulk',
+                  'Spiderman'
+                  ]
+
+videoGames = ['Overwatch (video game)',
+              'Uncharted',
+              'Devil May Cry',
+              ' Grand Theft Auto: San Andreas',
+              'Call of Duty: Black Ops',
+              'God of War (series)',
+              'Call of Duty: World at War',
+              'Minecraft',
+              "Assassin's Creed (video game)",
+              'God of War (video game)',
+              'Grand Theft Auto V'
+              'Fortnite'
+              ]
+
+
+
+nonFiction = [sports, entertainers, food]
+fiction = [movies, videoGames, fictional_char]
+
+print(nonFiction, '\n')
+print(fiction, '\n')
